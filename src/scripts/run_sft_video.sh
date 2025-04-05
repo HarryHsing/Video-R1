e00c0c14c762da787ed82deffabc6ebc27549cac
HarryHsing <zhenghao.xing@link.cuhk.edu.hk>
2025-04-05 20:15:28 +0800

    test

--- src/scripts/run_sft_video.sh
+++ src/scripts/run_sft_video.sh
@@ -11,9 +11,9 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node="4" \
     --master_port="12349" \
     src/open_r1/sft_video.py \
     --output_dir "./log/Qwen2.5-VL-7B-Video-7B-cot-sft" \
-    --model_name_or_path "Qwen/Qwen2.5-VL-7B-Instruct" \
-    --dataset_name "./Video-R1-data/Video-R1-COT-165k.json" \
-    --deepspeed local_scripts/zero2.json \
+    --model_name_or_path "/research/d1/gds/zhxing/projects_r1/models/Qwen2.5-VL-7B-Instruct" \
+    --dataset_name "/research/d1/gds/zhxing/projects_r1/datasets/Video-R1-data/Video-R1-COT-165k.json" \
+    --deepspeed local_scripts/zero3_offload.json \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 2 \
     --learning_rate 1e-6 \
@@ -24,6 +24,6 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node="4" \
     --attn_implementation flash_attention_2 \
     --num_train_epochs 1 \
     --run_name Qwen2.5-VL-7B-Video-cot-sft \
-    --save_steps 1000 \
+    --save_steps 100 \
     --max_grad_norm 5 \
     --save_only_model true \
